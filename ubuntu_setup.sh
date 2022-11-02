52c2c5bedbe6920b71fa172aa90a0b802d1a0ce3
madenney <matt.a.denney@gmail.com>
2022-11-02 14:29:11 -0700

    update

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,3 +1,9 @@
 
 
 sudo ubuntu-drivers autoinstall
+
+mkdir ~/Projects/sandbox
+cd ~/Projects/sandbox
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
